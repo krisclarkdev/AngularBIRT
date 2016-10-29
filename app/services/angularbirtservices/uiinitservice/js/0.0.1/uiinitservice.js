481183150f32e9eb669c133aa84a6a9b1a512bfa
Kristopher Clark <krisclarkdev@krisbox.org>
2016-10-29 14:15:47 -0500

    Removed old pagination code

--- app/services/angularbirtservices/uiinitservice/js/0.0.1/uiinitservice.js
+++ app/services/angularbirtservices/uiinitservice/js/0.0.1/uiinitservice.js
@@ -8,39 +8,8 @@ app.service('angularBirtUiInitService', function($http, $q) {
             window.dialogHelp   = $('#dialogHelp').dialog({autoOpen: false, zIndex: 10000});
             window.savedFilters = $('#dialogSavedFilters').dialog({autoOpen: false, zIndex: 10000, maxHeight: 450, minHeight: 450});
             window.savedFiltersGroup = $('#savedFiltersOutput').accordion();
-            paginationConstructor();
         });
-    }
-
-    var moveForward = function() {
-
-    }
-
-    function paginationConstructor() {
-        window.moveForwardOne = function() {
-            var next = myViewer.getCurrentPageNum() + 1;
-            if(next <= myViewer.getTotalPageCount()) {
-                $('#pageN').html(next);
-                myViewer.gotoPage(next);
-            }
-        }
-
-        window.moveBackwardOne = function() {
-            var prev = myViewer.getCurrentPageNum() - 1;
-            if(prev > 0) {
-                myViewer.gotoPage(prev);
-                $('#pageN').html(prev);
-            }
-        }
-
-        window.moveForwardAll = function() {
-            myViewer.gotoPage(myViewer.getTotalPageCount());
-        }
-
-        window.moveBackwardAll = function() {
-            myViewer.gotoPage(1);
-        }
-    }
+    };
 
     this.doInit = function() {
         init();
